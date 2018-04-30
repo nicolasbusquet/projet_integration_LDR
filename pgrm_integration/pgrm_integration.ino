aff2ab1bf63f5277292d12f184bac7ecf7c6518f
nicolasbusquet <37069232+nicolasbusquet@users.noreply.github.com>
2018-04-30 15:42:29 +0200

    modif prgrm
    1

--- pgrm_integration/pgrm_integration.ino
+++ pgrm_integration/pgrm_integration.ino
@@ -2,13 +2,8 @@
 
 typedef class 
 {
-    public:
-    CSurveillanceMesure(float valdefaut = 0);
-    void affichemesures();
-    bool mesurer();
-
-
-    private:
+      private:
+      
         float tabMesures[10];
         int nbMesures;
         void tritableau();
@@ -16,40 +11,44 @@ typedef class
 }CSurveillanceMesure;
 
 
-
-CSurveillanceMesure::CSurveillanceMesure(float valdefaut)
+void initialisermesures (CSurveillanceMesure *init)
 {
+  
   int i = 0; //compteur
+  int valdefaut = -1;
     while (i<10)
     {
-        this->tabMesures[i] = valdefaut; //on initialise le tableau de mesures avec la valeur par défaut
+        (*init).tabMesures[i] = valdefaut; //on initialise le tableau de mesures avec la valeur par défaut
         i++;
     }
-    this->nbMesures = 0; //on initialise le nombre de valeurs a 0
+    (*init).nbMesures = 0; //on initialise le nombre de valeurs a 0
 }
+    //void affichemesures();
+    //bool mesurer();
 
-bool CSurveillanceMesure::mesurer()
+
+
+bool mesurer(CSurveillanceMesure *msr)
 {
     int j = 1;
     
 
-    if (this->nbMesures == 10) //si le tableau est plein, il faut "supprimer" la premiere mesure du tableau et decaler les autres mesures
+    if ((*msr).nbMesures == 10) //si le tableau est plein, il faut "supprimer" la premiere mesure du tableau et decaler les autres mesures
     {
         while (j<10) //on parcourt tout le tableau
             {
-                this->tabMesures[j-1] = this->tabMesures[j]; //on remplace la case j-1 du tableau par la case j
+                (*msr).tabMesures[j-1] = (*msr).tabMesures[j]; //on remplace la case j-1 du tableau par la case j
                 j++; //compteur boucle
             }
-    this->tabMesures[9] = analogRead(A0); //on range la mesure rentree par l'utilisateur dans la derniere case du tableau
-    system("cls");
+    (*msr).tabMesures[9] = analogRead(A0); //on range la mesure rentree par l'utilisateur dans la derniere case du tableau
     return true; //tout s'est bien passe
     //il est important de noter que l'on incremente plus notre nombre de mesures, le tableau etant deja plein
     }
+    
     else //si le tableau n'est pas plein
     {
-        this->tabMesures[this->nbMesures] = analogRead(A0); //on range la mesure dans le tableau
-        this->nbMesures = this->nbMesures + 1; //on incremente le nombre de mesure
-        system("cls");
+        (*msr).tabMesures[(*msr).nbMesures] = analogRead(A0); //on range la mesure dans le tableau
+        (*msr).nbMesures = (*msr).nbMesures + 1; //on incremente le nombre de mesure
         return true; //tout s'est bien passe
     }
     return false; //quelque chose s'est mal deroule
@@ -60,20 +59,27 @@ bool CSurveillanceMesure::mesurer()
 
 void setup() {
 
-  // Initialise la communication avec le PC
-  Serial.begin(9600);
-}
+                // Initialise la communication avec le PC
+                 Serial.begin(9600);
+             }
 
 // Fonction loop(), appelée continuellement en boucle tant que la carte Arduino est alimentée
 void loop() {
 bool verif;
-CSurveillanceMesure mesure(-1);
+int test = 0;
 
-verif = mesure.mesurer();
+if (x == 0)
+{
+CSurveillanceMesure mesure;
+initialisermesures(&mesure);
+x = 1;
+}
+
+verif = mesurer(&mesure);
 while (verif==false)
      {
-        cout << "mesure echouee" << endl;
-        verif = mesure.mesurer();
+        serial.println("mesure echouee");
+        verif = mesurer(&mesure);
      }
   
   // Envoi la mesure au PC pour affichage et attends 250ms
